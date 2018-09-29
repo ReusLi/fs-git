22e37774493af967287d841088b4fcfd24b8c561
ReusLi <574399934@qq.com>
2018-09-29 23:05:19 +0800

    diff2html demo

---
+++ gitdiff.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+const diff2html = require('diff2html')
+
+let result = fs.readFileSync('saved.patch',
+    'utf-8')
+
+
+var cfg = {
+    "wordByWord": true,
+    "outputFormat": "side-by-side",
+    "matching": "lines",
+    "matchWordsThreshold": 0.25,
+    "matchingMaxComparisons": 2500,
+    "maxLineLengthHighlight": 10000,
+    "inputFormat": "diff",
+    "showFiles": true
+}
+let jsd = diff2html.Diff2Html.getJsonFromDiff(result, cfg)
+
+console.log(jsd)
